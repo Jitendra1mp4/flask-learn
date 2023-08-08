44a974603bd089a59d0f347f9a8b7b7a74ea8d2a
Jitendra Kumar <jitendrasahu6761@gmail.com>
2023-08-08 12:32:32 +0530

    sent data and iterated through it

--- app.py
+++ app.py
@@ -5,5 +5,16 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/home")
 def homepage() :
+  
     return render_template("homepage.html",username="jitendra")
 
+@app.route('/users') 
+def users_page () :
+      users = [
+                {'id':1,"name": "Jitendra Kumar", "phone" : "914355335", "address" : "Raipur"},
+                {'id':2,"name": "Fake Gurus", "phone" : "787554844", "address" : "Rajim"},
+                {'id':3,"name": "John Cal", "phone" : "954871554", "address" : "Navapara"},
+                {'id':4,"name": "Sita Kumar", "phone" : "684548754", "address" : "Abhanpur"}
+        ]
+      return render_template('users.html', users=users)
+      
