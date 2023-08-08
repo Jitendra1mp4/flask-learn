ed5a60071980ee25618c2f6178f9a729a9b69f64
Jitendra Kumar <jitendrasahu6761@gmail.com>
2023-08-08 11:10:28 +0530

    added example of dynamic route

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-from flask import Flask ,jsonify,request, render_template
+from flask import Flask #,jsonify,request, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+# decorator in python 
+@app.route("/") #where root of our website
 def home() : 
-        return "<h1>I think i would works</h1>"
+        return "<h1>I t it would not works</h1>"
+
+
+# this is called as dynamic route
+@app.route("/about/<username>")
+def about(username) :
+        return (f"Hello {username}!")
+
+ 
