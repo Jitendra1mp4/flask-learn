29f239cec47a58a66bf0cf68a930a456c19ed1ec
Jitendra Kumar <jitendrasahu6761@gmail.com>
2023-08-08 11:24:04 +0530

    successfully rendered  a template

--- app.py
+++ app.py
@@ -1,23 +1,8 @@
-# setting some environment variables 
-#cmd :  set FLASK_APP=app.py in windows and 
-#cmd : export FLASK_APP = app.py in linux
-# To turn the debug mod on we need to run following command 
-#cmd : set FLASK_DEBUG=1
-
-
-from flask import Flask #,jsonify,request, render_template
+from flask import Flask , render_template
 
 app = Flask(__name__)
 
-# decorator in python 
-@app.route("/") #where root of our website
-def home() : 
-        return "<h1>I t it would not works</h1>"
+@app.route("/")
+def homepage() :
+    return render_template("homepage.html")
 
-
-# this is called as dynamic route
-@app.route("/about/<username>")
-def about(username) :
-        return (f"Hello {username}!")
-
- 
